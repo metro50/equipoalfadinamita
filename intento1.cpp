8194ad9a808c2ee8b2ef0ab8a8410cc5c43a99be
metro50 <candemairamirez28@gmail.com>
2023-11-29 01:05:40 -0300

    avance-29-11

--- intento1.cpp
+++ intento1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include "matrix"
+#include <vector>
+#include <matrix>
 using namespace std;
 
 
@@ -24,7 +25,7 @@ class tablero {
     private:
         matrix<char> tab;
     public:
-        tablero() : tab(10,10,' ') {
+        tablero() : tab(16,16,' ') {
             
         }
         void poner_barquito( barquito barco) {
@@ -36,14 +37,14 @@ class tablero {
         }
         void mostrar_tablero() {
             std::cout << "UBICACIONES DE LOS BARQUITOS:" << endl;
-            std::cout << "-----------------------------------------" << endl;
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++) {
+            std::cout << "------------------------------------------------------------------" << endl;
+            for (int i = 0; i < 16; i++) {
+                for (int j = 0; j < 16; j++) {
                     if (j == 0) std::cout << "|";
                     std::cout << " " << tab[i][j] << " |";
                 }
                     std::cout << endl;
-                    std::cout << "-----------------------------------------" << endl;
+                    std::cout << "------------------------------------------------------------------" << endl;
             }
             std::cout << endl << endl;
         }
@@ -61,7 +62,7 @@ class ataques {
     private:
         matrix<char> bombitas;
     public:
-        ataques() : bombitas(10,10,' ') {}
+        ataques() : bombitas(16,16,' ') {}
 
         void AgregarAtaque( int posX, int posY, tablero jugador2 ) {
             matrix<char> tab2 = jugador2.obtener_tablero();
@@ -75,14 +76,14 @@ class ataques {
 
         void VerAtaques() {
             std::cout << "ATAQUES:" << endl;
-            std::cout << "-----------------------------------------" << endl;
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++) {
+            std::cout << "------------------------------------------------------------------" << endl;
+            for (int i = 0; i < 16; i++) {
+                for (int j = 0; j < 16; j++) {
                     if (j == 0) std::cout << "|";
                     std::cout << " " << bombitas[i][j] << " |";
                 }
                     std::cout << endl;
-                    std::cout << "-----------------------------------------" << endl;
+                    std::cout << "------------------------------------------------------------------" << endl;
             }
             std::cout << endl << endl;
         }
@@ -96,14 +97,21 @@ struct jugador {
     ataques ataq;
 };
 
+/*int contador(ataques a){
+	int cont=0;
+	for(int i=0;i<a.size(0);i++){
+		for(int j=0;j<a.size(1);j++){
+			if (a[i][j]=='A'){
+				cont++;
+			}
+		}
+	}
+return cont;	
+}
+	*/
+
 int main() {
 	
-    int alto = 2, 
-        ancho = 3, 
-        xPos = 2, 
-        yPos = 6;
-
-
     tablero tb1;
     tablero tb2;
     ataques ataq1;
@@ -112,15 +120,29 @@ int main() {
     jugador ian = { "Ian", 26, tb2, ataq2};
 
 
-    barquito barquito1 = {alto, ancho, xPos, yPos};
+   /* barquito barquito1 = {alto, ancho, xPos, yPos};
     knd.tab.poner_barquito(barquito1);
     knd.tab.mostrar_tablero();
 
     ian.ataq.AgregarAtaque(2, 6, knd.tab);
     ian.ataq.VerAtaques();
+*/
+
+vector<jugador> jugadores ={ian, knd};
+for(jugador &jr:jugadores){
+		
+	jr.tab.poner_barquito({4,1,0,10});
+	jr.tab.poner_barquito({6, 1, 2, 8});
+	jr.tab.poner_barquito({5, 1, 4, 9});
+	jr.tab.poner_barquito({5, 2, 6, 9});
+	jr.tab.poner_barquito({3, 1, 9, 11});
+	jr.tab.poner_barquito({4, 1, 11, 10});
+	jr.tab.mostrar_tablero();
+}
 
 
 
+	
 	return 0;
 }
 
